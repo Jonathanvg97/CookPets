24c870e91f495e767a4abd64bd529a62d2fabeb9
Jonathan Vanegas <jonathanvanegasg@hotmail.com>
2022-12-27 00:49:34 -0500

    subiendo archivo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './App.css';
-import Home from './Components/Home/Home'
+import Home from './Components/Home/Home';
 import Menu from './Components/Menu/Menu';
-import Contacts from './Components/Contacts/Contacts';
+
 
 
 function App() {
   return (
-    <div className="App"> 
+   
     <>
     <Home/>
     <Menu/>
-    <Contacts/>
     </>
-    </div>
   );
 }
 
