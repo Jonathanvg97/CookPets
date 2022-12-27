9e1132cc9a1e87f7a9d7d1fcd1d36f815571bb45
Jonathan Vanegas <jonathanvanegasg@hotmail.com>
2022-12-27 00:40:54 -0500

    subindo

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./productCard.css";
 
-
 const ProductCard = (props) => {
   return (
     <>
-    
       <div className="card">
         <h2>{props.title}</h2>
         <img src={props.url} alt="Imagenes Productos" />
         <h3>{props.cantidad}</h3>
         <ul className="Size">
-        <button><li>{props.pequeña}</li></button>
-        <button><li>{props.mediana}</li></button>
-        <button><li>{props.grande}</li></button>
+          <button>
+            <li>{props.pequeña}</li>
+          </button>
+          <button>
+            <li>{props.mediana}</li>
+          </button>
+          <button>
+            <li>{props.grande}</li>
+          </button>
         </ul>
-        <h3>{props.price}</h3>  
+        <h3>{props.price}</h3>
       </div>
     </>
   );
