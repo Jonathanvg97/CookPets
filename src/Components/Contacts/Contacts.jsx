458ec4d452f2b698738df49e503cf80f989705ff
Jonathan Vanegas <jonathanvanegasg@hotmail.com>
2022-12-30 12:07:47 -0500

    Subiendo estilos y funcionalidad carrito

--- src/Components/Contacts/Contacts.jsx
+++ src/Components/Contacts/Contacts.jsx
@@ -1,200 +1,74 @@
-// import { useForm } from "react-hook-form";
-// import './contacts.css'
+import { useForm } from "react-hook-form";
+import './contacts.css'
 
 
-// const Contactanos = () => {
+const Contactanos = () => {
 
-//     const { register, formState: { errors },handleSubmit } = useForm({
-//         defaultValues: {
-//             nombre: '',
-//             direccion: ''
-//         }
-//     });
+    const { register, formState: { errors },handleSubmit } = useForm({
+        defaultValues: {
+            nombre: '',
+            direccion: ''
+        }
+    });
 
-//     // const App = () => (
-//     //     <ReactWhatsapp number="57 3108580916" message="<value>" />
-//     //   );
+    // const App = () => (
+    //     <ReactWhatsapp number="57 3108580916" message="<value>" />
+    //   );
       
-//     //   render(<App />, document.getElementById('root'));
+    //   render(<App />, document.getElementById('root'));
 
-//     const onSubmit = (data) => {
-//         console.log(data);
-//     }
+    const onSubmit = (data) => {
+        console.log(data);
+    }
 
-//     return <div>
-//         <h1 className="Form">Formulario</h1>
-//         <form onSubmit={handleSubmit(onSubmit)}>
-//             <div>
-//                 <label>Nombre</label>
-//                 <input type="text" {...register('nombre', {
-//                     required: true,
-//                     maxLength: 20
-//                 })} />
-//                 {errors.nombre?.type === 'required' && <p>El campo nombre es requerido</p>}
-//                 {errors.nombre?.type === 'maxLength' && <p>El campo nombre debe tener menos de 20 caracteres</p>}
-//             </div>
-//             <div>
-//                 <label>Dirección</label>
-//                 <input type="text" {...register('direccion', {
-//                     required: true
-//                 })} />
-//             </div>
-//             <div>
-//                 <label>Email</label>
-//                 <input type="text" {...register('email', {
-//                     pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i
-//                 })} />
-//                 {errors.email?.type === 'pattern' && <p>El formato del email es incorrecto</p>}
-//             </div>
-//             <div>
-//                 <label>Mascota</label>
-//                 <select {...register('Mascota')}>
-//                     <option value="Dog">Perro</option>
-//                     <option value="Cat">Gato</option>
-//                 </select>
-//             </div>
-//             <div>
-//                 <label>Cuentanos</label>
-//                 <input type="text"{...register('Cuentanos', {
-//                     required: true,
-//                 })} />
-//                     {errors.nombre?.type === 'required' && <p>El campo cuentanos es requerido</p>}
-//             </div>
+    return <div>
+        <h1 className="Form">Formulario</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label>Nombre</label>
+                <input type="text" {...register('nombre', {
+                    required: true,
+                    maxLength: 20
+                })} />
+                {errors.nombre?.type === 'required' && <p>El campo nombre es requerido</p>}
+                {errors.nombre?.type === 'maxLength' && <p>El campo nombre debe tener menos de 20 caracteres</p>}
+            </div>
+            <div>
+                <label>Dirección</label>
+                <input type="text" {...register('direccion', {
+                    required: true
+                })} />
+            </div>
+            <div>
+                <label>Email</label>
+                <input type="text" {...register('email', {
+                    pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i
+                })} />
+                {errors.email?.type === 'pattern' && <p>El formato del email es incorrecto</p>}
+            </div>
+            <div>
+                <label>Mascota</label>
+                <select {...register('Mascota')}>
+                    <option value="Dog">Perro</option>
+                    <option value="Cat">Gato</option>
+                </select>
+            </div>
+            <div>
+                <label>Cuentanos</label>
+                <input type="text"{...register('Cuentanos', {
+                    required: true,
+                })} />
+                    {errors.nombre?.type === 'required' && <p>El campo cuentanos es requerido</p>}
+            </div>
         
             
           
-//             <input type="submit" value="Enviar Whatsapp" />
-//         </form>
-//     </div>
-// }
+            <input type="submit" value="Enviar Whatsapp" />
+        </form>
+    </div>
+}
 
 
 
-// export default Contactanos;
+export default Contactanos;
 
-// const Contacts = () => {
-//     const CHARACTER_LIMIT = 100;
-  
-//     const [numberEmptyError, setNumberEmptyError] = useState(false);
-//     const [messageEmptyError, setMessageEmptyError] = useState(false);
-  
-//     const [formData, setFormData] = useState({
-//       mobileNumber: "",
-//       message: "",
-//     });
-  
-//     const { mobileNumber, message } = formData;
-  
-//     const onChange = (e) => {
-//       e.preventDefault();
-//       setFormData({
-//         ...formData,
-//         [e.target.name]: e.target.value,
-//       });
-//     };
-  
-//     const onSubmit = (e) => {
-//       e.preventDefault();
-//       if (mobileNumber.length < 1) {
-//         setNumberEmptyError(true);
-//         setTimeout(() => setNumberEmptyError(false), 3000);
-//       } else if (message.length < 1) {
-//         setMessageEmptyError(true);
-//         setTimeout(() => setMessageEmptyError(false), 3000);
-//       } else {
-     
-//         // TODO: Enter code here
-//       }
-//     };
-  
-//     return (
-//       <div className='communication'>
-//         <div className='whatsapp-card app'>
-//           <div className='title flex_middle'>
-//             <div style={{ marginRight: "0.5em" }}>
-//               <WhatsAppIcon />
-//             </div>
-//             <div>Send Message</div>
-//           </div>
-//           {numberEmptyError && (
-//             <div className='errors'>Mobile number cannot be empty!</div>
-//           )}
-//           {messageEmptyError && (
-//             <div className='errors'>Message cannot be empty!</div>
-//           )}
-//           {!numberEmptyError && !messageEmptyError && (
-//             <div className='errors-null'>.</div>
-//           )}
-//           <div className='search_contact app'>
-//             <CssTextField
-//               error={numberEmptyError}
-//               label='Mobile Number'
-//               placeholder='Mobile Number'
-//               name='mobileNumber'
-//               value={mobileNumber}
-//               onChange={onChange}
-//               size='small'
-//               style={{
-//                 margin: "1em 0em",
-//               }}
-//               inputProps={{
-//                 style: textFieldStyle,
-//               }}
-//               InputLabelProps={{
-//                 style: textFieldInputLabelStyle,
-//               }}
-//               required
-//             />
-//           </div>
-//           <div className='message app'style={{marginTop: "1.5em"}}>
-//             <CssTextField
-//               multiline
-//               maxRows={4}
-//               label='Message'
-//               placeholder='Hi! Sending a message from React....'
-//               size='small'
-//               InputLabelProps={{
-//                 style: textFieldInputLabelStyle,
-//               }}
-//               inputProps={{
-//                 style: {
-//                   width: "230px",
-//                   height: "90px",
-//                 },
-//                 maxLength: CHARACTER_LIMIT,
-//               }}
-//               FormHelperTextProps={{
-//                 style: {
-//                   margin: 0,
-//                   padding: "0 0 0 5px",
-//                   fontSize: 10,
-//                 },
-//               }}
-//               name='message'
-//               value={message}
-//               onChange={onChange}
-//               required
-//               error={message.length > CHARACTER_LIMIT - 1 || messageEmptyError}
-//               helperText={
-//                 !(message.length > CHARACTER_LIMIT - 1)
-//                   ? `${message.length}/${CHARACTER_LIMIT}`
-//                   : "Max length exceeded"
-//               }
-//             />
-//           </div>
-//           <div style={{marginTop:'1.5em'}}>
-//             <Button
-//               onClick={onSubmit}
-//               variant='outlined'
-//               color='success'
-//               size='small'
-//             >
-//               Send
-//             </Button>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   };
-
-//   export default Contacts
